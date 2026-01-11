ccb36996f5689de0fe794c1f7433a82a9453b7c6
juanib99 <juanignaciobisello@hotmail.com>
2026-01-11 00:05:12 -0300

    ticket unificados

--- src/routes/ticket.routes.ts
+++ src/routes/ticket.routes.ts
@@ -5,11 +5,12 @@ import { authMiddleware } from "../middlewares";
 const router = Router();
 const controller = TicketsController.getInstance();
 
+// Todas las rutas usan authMiddleware para proteger los datos de la óptica
 router.post('/', authMiddleware, controller.createTicket.bind(controller));
 router.get('/', authMiddleware, controller.getTickets.bind(controller));
 router.get('/:id', authMiddleware, controller.getTicketById.bind(controller));
-router.put('/:id', authMiddleware, controller.updateTicket.bind(controller));
-router.delete('/:id', authMiddleware, controller.deleteTicket.bind(controller));
-router.post('/:id/entregar', authMiddleware, controller.entregarTicket.bind(controller));
+
+router.put('/:id/status', authMiddleware, controller.updateStatus.bind(controller));
+router.post('/:id/deliver', authMiddleware, controller.deliverTicket.bind(controller));
 
 export default router;
