3b1745cf88f797a2c9252244ea18e9f469a8b7da
juanib99 <juanignaciobisello@hotmail.com>
2026-02-10 11:02:57 -0300

    problemas de auth

--- src/routes/tenants.routes.ts
+++ src/routes/tenants.routes.ts
@@ -1,20 +1,23 @@
 import { Router } from 'express';
 import { TenantsController } from '../controllers/tenants.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
-import { isSuperAdmin } from '../middlewares/isSuperAdmin';
+import { isAdmin, isSuperAdmin } from '../middlewares';
 
 const router = Router();
 const controller = TenantsController.getInstance();
 
+// 1. Middleware base para todos (deben estar logueados)
 router.use(authMiddleware);
-router.use(isSuperAdmin);
 
-router.post('/', controller.createTenant.bind(controller));
+// 2. Rutas Públicas para usuarios logueados (Cualquier rol)
 router.get('/', controller.getTenants.bind(controller));
-router.post('/user-branch', controller.changeUserBranch.bind(controller));
 router.get('/:id', controller.getTenantById.bind(controller));
-router.put('/:id', controller.updateTenant.bind(controller));
-router.delete('/:id', controller.deleteTenant.bind(controller));
 router.get('/search/:nombre', controller.searchTenantByName.bind(controller));
+
+// 3. Rutas Restringidas (Solo SuperAdmin)
+router.post('/', isSuperAdmin, controller.createTenant.bind(controller));
+router.put('/:id', isSuperAdmin, controller.updateTenant.bind(controller));
+router.delete('/:id', isSuperAdmin, controller.deleteTenant.bind(controller));
+router.post('/user-branch', isAdmin, controller.changeUserBranch.bind(controller));
 
 export default router;
